3a15562a2904995faf681ef0eafdf59b0211cb3a
Hitman 47 <h401040@gmail.com>
2026-05-30 18:08:50 +0000

    fix: Correct middleware import path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,9 @@
-
-import { authMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ["/", "/quiz/:id*"],
+  publicRoutes: ['/', '/quiz/:id*'],
 });
 
 export const config = {
-  matcher: ["/((?!.+//.[w]+$|_next).)", "/", "/(api|trpc)(.)"],
+  matcher: ['/((?!.+//.[w]+$|_next).)', '/', '/(api|trpc)(.)'],
 };
